fca8d04e33cc7c117d2d8963055114fb408dc5b1
Boyko <bmlalov@abv.bg>
2015-10-26 09:38:30 +0200

    add databse folder to project

--- DependenciesResolving/src/com/boyko/repository/PathsRepository.java
+++ DependenciesResolving/src/com/boyko/repository/PathsRepository.java
@@ -3,7 +3,7 @@ package com.boyko.repository;
 import java.io.File;
 
 public class PathsRepository {
-	public static final File PATH_TO_PACKAGES = new File("/home/boyko/Documents/Java/npmPackages/packages");
+	public static final File PATH_TO_PACKAGES = new File("database");
 	public static final File PATH_TO_DEPENDENCIES = new File("lib/dependencies.json");
 	public static final String PATH_TO_INSTALLED_MODULES = "lib/installed_modules/";
 }
